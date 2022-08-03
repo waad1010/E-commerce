8740e00e021c44b1d3d74f6ce16eda92c260c8d4
anask44 <anas.kaffini@gmail.com>
2022-08-03 12:46:23 +0300

    merged back into folder

---
+++ back/Config/dbConf.js
@@ -0,0 +1,29 @@
+const Config= {
+    
+    database:"eComerce",
+
+    server:"es-sys521",
+
+    driver:"msnodesqlv8",
+
+    user : "PSSTrainee9",
+
+    options:{
+
+        trustServerCertificate:true,
+
+        trustedConnection:true,
+
+       
+
+
+
+    },
+
+
+}
+
+
+
+
+module.exports = Config;
