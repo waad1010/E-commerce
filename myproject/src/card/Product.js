7b2428aaa04f19d8b5eb8a0f35a4036df0f85e33
waad1010 <https://github.com/waad1010/project_estarta.git>
2022-08-07 12:13:33 +0300

    Merge branch 'main' of https://github.com/waad1010/e-commerce into waed fixed conflicts

--- myproject/src/card/Product.js
+++ myproject/src/card/Product.js
@@ -21,14 +21,6 @@ export default function Product(props) {
        });
     };
   return (
-<<<<<<< HEAD
-    <span>
-      <img className="small" src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <div>${props.price}</div>
-      <div><Form onAddToCart ={addHandler}/></div>
-    </span>
-=======
   <div class="a-box">
     <Link to ='/'>
   <div class="img-container">
@@ -49,10 +41,5 @@ export default function Product(props) {
   <div ><Form onAddToCart ={addHandler}/></div>
 </div>
 </div>
-  
-  
-  
- 
->>>>>>> c1325552123198fd2625500c1dfefc4dc0e3e6ba
   );
 }
